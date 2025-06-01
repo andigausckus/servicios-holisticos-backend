0b16647ba1a367df03019eb2d8264e9b865a1e02
andigausckus <andigausckus36@gmail.com>
2025-06-01 18:15:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+app.use(cors());
 const cors = require("cors");
 
 app.use(express.json());
