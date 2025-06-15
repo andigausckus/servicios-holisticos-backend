02d6866003f5a88e08b2b8cb20874095eee61d91
andigausckus <andigausckus36@gmail.com>
2025-06-14 22:03:16 -0300

    Update servicios.js

--- routes/servicios.js
+++ routes/servicios.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../auth"); // si querés proteger con token
+const auth = require("../auth");
 const subirImagen = require("../middlewares/subirImagen");
 const { crearServicio } = require("../controladores/servicios");
 
