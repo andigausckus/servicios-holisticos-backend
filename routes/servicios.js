4f246a5d2ffddd07c9c140294c3fec63a112999c
andigausckus <andigausckus36@gmail.com>
2025-06-15 16:21:34 -0300

    Update servicios.js

--- routes/servicios.js
+++ routes/servicios.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../auth");
-const subirImagen = require("../middlewares/subirImagen");
 const { crearServicio } = require("../controladores/servicios");
 
-router.post("/", auth, subirImagen, (req, res, next) => {
-  console.log("👉 Llegó un POST a /api/servicios");
+router.post("/", auth, (req, res, next) => {
+  console.log("👉 Llegó un POST a /api/servicios sin imagen");
   next();
 }, crearServicio);
 
