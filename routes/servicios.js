d4d48ccb6e44f4bcc4b89319d749dcfb43b8f7df
andigausckus <andigausckus36@gmail.com>
2025-06-15 11:36:31 -0300

    Update servicios.js

--- routes/servicios.js
+++ routes/servicios.js
@@ -5,7 +5,7 @@ const subirImagen = require("../middlewares/subirImagen");
 const { crearServicio } = require("../controladores/servicios");
 
 router.post("/", auth, subirImagen, crearServicio);
-// En routes/servicios.js
+
 router.get("/", (req, res) => {
   res.send("✅ Ruta de servicios activa");
 });
