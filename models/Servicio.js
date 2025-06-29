13f16d3a459b06d69f0b4b64577749a28ecf1ee4
andigausckus <andigausckus36@gmail.com>
2025-06-29 19:37:26 -0300

    Update Servicio.js

--- models/Servicio.js
+++ models/Servicio.js
@@ -6,7 +6,7 @@ const servicioSchema = new mongoose.Schema({
   modalidad: {
     type: [String], // Array como ["Online", "Presencial"]
     enum: ["Presencial", "Online"],
-    required: true
+    required: true,
   },
   ubicacion: {
     type: String,
@@ -15,18 +15,23 @@ const servicioSchema = new mongoose.Schema({
         // Solo es obligatorio si incluye "Presencial"
         return this.modalidad.includes("Presencial") ? !!valor : true;
       },
-      message: "La ubicación es obligatoria para servicios presenciales."
-    }
+      message: "La ubicación es obligatoria para servicios presenciales.",
+    },
   },
   duracion: { type: Number, required: true }, // minutos totales
   precio: { type: Number, required: true },
   categoria: { type: String, required: true },
+  plataformas: {
+    type: [String],
+    enum: ["WhatsApp", "Zoom", "Skype", "Google Meet"],
+    required: true,
+  },
   imagen: { type: String },
   terapeuta: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Terapeuta",
-    required: true
-  }
+    required: true,
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Servicio", servicioSchema);
