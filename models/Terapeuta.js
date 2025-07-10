209845888c70137e38192944e95a138d2c7da694
andigausckus <andigausckus36@gmail.com>
2025-07-10 14:02:39 -0300

    Update Terapeuta.js

--- models/Terapeuta.js
+++ models/Terapeuta.js
@@ -59,10 +59,6 @@ const TerapeutaSchema = new mongoose.Schema({
     required: [true, "La contraseña es obligatoria"],
     minlength: [6, "La contraseña debe tener al menos 6 caracteres"]
   },
-  especialidades: {
-    type: String,
-    required: [true, "Al menos una especialidad es obligatoria"]
-  },
   whatsapp: {
   type: String,
   required: [true, "El número de WhatsApp es obligatorio"],
