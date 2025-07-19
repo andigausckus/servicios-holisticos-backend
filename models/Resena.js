97c80c5229ec44d979fc38d679fab066f6f21d5d
andigausckus <andigausckus36@gmail.com>
2025-07-19 10:31:08 -0300

    Update Resena.js

--- models/Resena.js
+++ models/Resena.js
@@ -1,10 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const resenaSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  comentario: { type: String, required: true },
-  aprobada: { type: Boolean, default: false },
-  fecha: { type: Date, default: Date.now },
-});
+const resenaSchema = new mongoose.Schema(
+  {
+    terapeuta: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Terapeuta",
+      required: true,
+    },
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    comentario: {
+      type: String,
+      trim: true,
+    },
+    puntaje: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    aprobado: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Resena', resenaSchema);
+module.exports = mongoose.model("Resena", resenaSchema);
