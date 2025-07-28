71569ffcbaa4f32c45da041e102be3cce6285d29
andigausckus <andigausckus36@gmail.com>
2025-07-28 12:48:53 -0300

    Update bloqueos.js

--- routes/bloqueos.js
+++ routes/bloqueos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Bloqueo = require("../models/Bloqueo");
 const Reserva = require("../models/Reserva");
-const Pusher = require("pusher");
+const pusher = require("../utils/pusher");
 
 // Crear bloqueo
 router.post("/", async (req, res) => {
