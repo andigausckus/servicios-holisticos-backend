f135b1760397017ee2b475fc64f3995ddb65491e
andigausckus <andigausckus36@gmail.com>
2025-07-30 13:12:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Arranque del servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
