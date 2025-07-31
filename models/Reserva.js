5f7a4d0a615b8892b9180213d7fb95c42c4c2ac7
andigausckus <andigausckus36@gmail.com>
2025-07-31 10:42:01 -0300

    Update Reserva.js

--- models/Reserva.js
+++ models/Reserva.js
@@ -15,7 +15,7 @@ const reservaSchema = new mongoose.Schema({
     enum: ["en_proceso", "pendiente_de_aprobacion", "confirmada", "cancelada"],
     default: "en_proceso",
   },
-  comprobantePago: { type: String, default: "" },
+  comprobantePago: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Reserva", reservaSchema);
