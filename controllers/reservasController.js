e1cfd6e1b789b43f983c0fbad373efc7bb7b268e
andigausckus <andigausckus36@gmail.com>
2025-08-01 11:07:47 -0300

    Update reservasController.js

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -37,7 +37,7 @@ const crearReservaConComprobante = async (req, res) => {
   comprobantePago,
   precio,
   duracion,
-  estado: "en_proceso",
+  estado: "pendiente",
 });
 
     await nuevaReserva.save();
